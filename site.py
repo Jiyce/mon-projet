c98d7476caeb54af3829db23b0ce4b5dd672e61f
Jiyce <joycetadaah26@gmail.com>
2026-04-19 21:01:48 +0000

    Add files via upload

--- site.py
+++ site.py
@@ -26,10 +26,6 @@ class MonSiteWeb(object):
                  Cette application a pour but d'évaluer les connaissances en matière de  santé sexuelle et reproductive chez les jeunes. Les données collectées sont anonymes et serviront à des fins éducatives.
                 </p>
         
-                <p>
-                    Cliquez sur le boutton pour commencer le questionnaire.
-                </p>
-        
                 <a href="questionnaire">
                     <button>Commencer le questionnaire</button>
                 </a>
@@ -43,35 +39,47 @@ class MonSiteWeb(object):
     @cherrypy.expose
     def questionnaire(self):
         return '''
+        <html>
+        <head>
+        <link rel="stylesheet" type="text/css" href="/static/style.css">
+        </head>
+        
+        <body>
+        <div class="container">
+        
         <h1><i>Questionnaire de santé sexuelle et reproductive</i></h1>
         <p>Veuillez répondre aux questions suivantes :</p>
         
         <form method="post" action="submit">
         
-        Age : <select name="age" required>
+        <label>Age : </label>
+            <select name="age" required>
                 <option value="15-19">15-19 ans</option>
                 <option value="20-25">20-25 ans</option>
                 <option value="25-30">25-30 ans</option>
                 <option value="30+">30 ans et plus</option>
             </select><br><br>
             
-        Sexe :
+        <label>Sexe : </label>
             <select name="sexe" required>
                 <option value=" Homme">Homme</option>
                 <option value="Femme">Femme</option>
             </select><br><br>
             
-        Avez-vous déjà participez à une campagne de sensibilisation sur la santé sexuelle et reproductive ?
+        <label>Avez-vous déjà participez à une campagne de sensibilisation sur la santé sexuelle et reproductive ?</label>
         <input type="radio" id="oui" name="sensibilisation" value="Oui" >Oui
         <input type="radio" id="non" name="sensibilisation" value="Non" >Non<br><br>
         
-        Etes_vous sexuellement actif ?
+        <label>Etes_vous sexuellement actif ?</label>
         <input type="radio" id="oui" name="dernier_rapport" value="Oui" required>
         <label for="oui">Oui</label>
         <input type="radio" id="non" name="dernier_rapport" value="Non" required>
         <label for="non">Non</label><br><br>
+        
+        <label>Age du premier rapport sexuel :</label>
+            <input type="number" name="age_premier_rapport" min="10" max="50" required><br><br>
             
-        Combien de partenaires sexuels avez-vous eu au cours de votre vie ? :
+        <label>Combien de partenaires sexuels avez-vous eu au cours de votre vie ? :</label>
             <select name="partenaires" required>
                 <option value="0">0</option>
                 <option value="1">1</option>
@@ -79,7 +87,7 @@ class MonSiteWeb(object):
                 <option value="5+">5 et plus</option>
             </select><br><br>
             
-        Quelle méthode de contraception utilisez-vous ?
+        <label>Quelle méthode de contraception utilisez-vous ?</label>
             <select name="contraception" required>
                 <option value="Aucune">Aucune</option>
                 <option value="Préservatif">Préservatif</option>
@@ -88,44 +96,61 @@ class MonSiteWeb(object):
                 <option value="Autre">Autre</option>
             </select><br><br>
             
-        Avez-vous déjà été testé pour les infections sexuellement transmissibles (ist) ?
+        <label>Avez-vous déjà été testé pour les infections sexuellement transmissibles (ist) ?</label>
         <input type="radio" id="oui" name="ist" value="Oui" required>
         <label for="oui">Oui</label>
         <input type="radio" id="non" name="ist" value="Non" required>
         <label for="non">Non</label><br><br> 
             
-        Le préservatif est-il efficace pour prévenir les grossesses et les ist ?
+        <label>Le préservatif est-il efficace pour prévenir les grossesses et les ist ?</label>
         <input type="radio" id="oui" name="preservatif" value="Oui" required>
         <label for="oui">Oui</label><br>
         <input type="radio" id="non" name="preservatif" value="Non" required>
         <label for="non">Non</label><br><br>
             
-        Quelles sont les différentes maladies sexuellement transmissibles (mst) que vous connaissez ?
-             <select name="mst" required>
-                <option value="Aucune">Aucune</option>
-                <option value="Chlamydia">Chlamydia</option>
-                <option value="Gonorrhée">Gonorrhée</option>
-                <option value="Syphilis">Syphilis</option>
-                <option value="VIH/SIDA">VIH/SIDA</option>
-                <option value="Autre">Autre</option>
-            </select><br><br>
-            
-        Comment amélioreriez-vous l'accès à l'éducation sexuelle et aux services de santé reproductive pour les jeunes ?
+        <label>Quelles sont les différentes maladies sexuellement transmissibles (mst) que vous connaissez ?</label>
+            <input type="text" name="mst" required><br><br>
+        
+        <label>Avez-vous déjà consulter un professionnel de santé?</label>
+           <input type="radio" id="oui" name="acces_sante" value="Oui" required>
+            <label for="oui">Oui</label>
+            <input type="radio" id="non" name="acces_sante" value="Non" required>
+            <label for="non">Non</label><br><br>
+        
+        <label>Utilisez-vous toujours une protection lors de vos rapports sexuels ?</label>
+            <input type="radio" id="oui" name="protection" value="Oui" required>
+            <label for="oui">Oui</label>
+            <input type="radio" id="non" name="protection" value="Non" required>
+            <label for="non">Non</label><br><br>
+        
+        <label>Pensez-vous que l'éducation sexuelle est suffisante ?</label>
+            <input type="radio" id="oui" name="education" value="Oui" required>
+            <label for="oui">Oui</label>
+            <input type="radio" id="non" name="education" value="Non" required>
+            <label for="non">Non</label><br><br>
+
+         <label>Comment amélioreriez-vous l'accès à l'éducation sexuelle et aux services de santé reproductive pour les jeunes ?</label>
         <input type="text" name="amelioration" required><br><br>
             
         <input type="submit" value="Soumettre">         
         </form>
+        
+        </div>
+        </body>
+        </html>
         '''
     # Traitement des données du formulaire
     
     @cherrypy.expose
-    def submit(self,age, sexe, sensibilisation,dernier_rapport,contraception,mst, amelioration, ist, preservatif, partenaires):
+    def submit(self,age, sexe, sensibilisation,dernier_rapport,contraception,mst, amelioration, ist, preservatif, partenaires,
+                   age_premier_rapport, acces_sante, protection, education):
         
         
         conn = self.connexion_db()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO reponses (age, sexe, sensibilisation,dernier_rapport,contraception,mst, amelioration, ist, preservatif, partenaires) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-                       (age, sexe, sensibilisation,dernier_rapport,contraception,mst, amelioration, ist, preservatif, partenaires))
+        cursor.execute("INSERT INTO reponses (age, sexe, sensibilisation,dernier_rapport,contraception,mst, amelioration, ist, preservatif, partenaires, age_premier_rapport, acces_sante, protection, education) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                       (age, sexe, sensibilisation,dernier_rapport,contraception,mst, amelioration, ist, preservatif, 
+                        partenaires, age_premier_rapport, acces_sante, protection, education))
         conn.commit()
         conn.close()
         
