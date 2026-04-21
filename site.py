69a3e88e6b1395195091c46e223be30b9a123eba
Jiyce <joycetadaah26@gmail.com>
2026-04-21 06:48:34 +0000

    Add files via upload

--- site.py
+++ site.py
@@ -16,9 +16,53 @@ class MonSiteWeb(object):
         <!DOCTYPE html>
         <html lang="fr">
         <head>
-           <title>ACCUEIL</title>
-           <link rel="stylesheet" type="text/css" href="/static/style.css">
+        <title>ACCUEIL</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background: linear-gradient(to right, #4facfe, #00f2fe);
+                margin: 0;
+                padding: 0;
+                text-align: center;
+            }
+
+            .container {
+                max-width: 600px;
+                margin: 50px auto;
+                background-color: #fff;
+                padding: 20px;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0,0,0,0.2);
+            }
+
+            h1 {
+                text-align: center;
+                color: #10037e;
+            }
+
+            p {
+                text-align: center;
+                color: #555;
+            }
+            
+             button {
+            background: #4facfe;
+            border: none;
+            padding: 12px 25px;
+            color: white;
+            font-size: 16px;
+            border-radius: 8px;
+            cursor: pointer;
+            transition: 0.3s;
+            }
+            
+            button:hover {
+            background: #007bff;
+            }
+
+        </style>
         </head>
+        
         <body>
             <div class="container">
                 <h1><i>SANTE SEXUELLE ET REPRODUCTIVE</i></h1>
@@ -41,7 +85,68 @@ class MonSiteWeb(object):
         return '''
         <html>
         <head>
-        <link rel="stylesheet" type="text/css" href="/static/style.css">
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background: linear-gradient(to right, #4facfe, #00f2fe);
+                margin: 0;
+                padding: 0;
+                text-align: center;
+            }
+
+            .container {
+                max-width: 600px;
+                margin: 50px auto;
+                background-color: #fff;
+                padding: 20px;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0,0,0,0.2);
+            }
+
+            h1 {
+                text-align: center;
+                color: #10037e;
+                
+            }
+
+            p {
+                text-align: center;
+                color: #555;
+                font-size: 18px;
+            }
+
+            label {
+                font-weight: bold;
+            }
+
+            input, select {
+                width: 100%;
+                padding: 10px;
+                margin: 10px 0 20px 0;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+            }   
+
+            input[type="radio"] {
+                width: auto;
+                margin-right: 10px;
+            }
+
+            button, input[type="submit"] {
+                background-color: #10037e;
+                color: white;
+                padding: 10px 20px;
+                border: none;
+                width: 100%;
+                border-radius: 4px;
+                cursor: pointer;
+            }
+
+            button:hover, input[type="submit"]:hover {
+                background-color: #00c6ff;
+            }
+            
+        </style>
         </head>
         
         <body>
@@ -52,6 +157,8 @@ class MonSiteWeb(object):
         
         <form method="post" action="submit">
         
+        <ol>
+        <li>
         <label>Age : </label>
             <select name="age" required>
                 <option value="15-19">15-19 ans</option>
@@ -59,26 +166,36 @@ class MonSiteWeb(object):
                 <option value="25-30">25-30 ans</option>
                 <option value="30+">30 ans et plus</option>
             </select><br><br>
-            
+        </li>
+        
+        <li>
         <label>Sexe : </label>
             <select name="sexe" required>
                 <option value=" Homme">Homme</option>
                 <option value="Femme">Femme</option>
             </select><br><br>
-            
+        </li>
+        
+        </li>    
         <label>Avez-vous déjà participez à une campagne de sensibilisation sur la santé sexuelle et reproductive ?</label>
         <input type="radio" id="oui" name="sensibilisation" value="Oui" >Oui
         <input type="radio" id="non" name="sensibilisation" value="Non" >Non<br><br>
+        </li>
         
+        <li>
         <label>Etes_vous sexuellement actif ?</label>
         <input type="radio" id="oui" name="dernier_rapport" value="Oui" required>
         <label for="oui">Oui</label>
         <input type="radio" id="non" name="dernier_rapport" value="Non" required>
         <label for="non">Non</label><br><br>
+        </li>
         
+        <li>
         <label>Age du premier rapport sexuel :</label>
             <input type="number" name="age_premier_rapport" min="10" max="50" required><br><br>
-            
+        </li>
+        
+        <li>
         <label>Combien de partenaires sexuels avez-vous eu au cours de votre vie ? :</label>
             <select name="partenaires" required>
                 <option value="0">0</option>
@@ -86,7 +203,9 @@ class MonSiteWeb(object):
                 <option value="2-5">2-5</option>
                 <option value="5+">5 et plus</option>
             </select><br><br>
-            
+        </li>
+        
+        <li>
         <label>Quelle méthode de contraception utilisez-vous ?</label>
             <select name="contraception" required>
                 <option value="Aucune">Aucune</option>
@@ -95,42 +214,59 @@ class MonSiteWeb(object):
                 <option value="Stérilet">Stérilet</option>
                 <option value="Autre">Autre</option>
             </select><br><br>
-            
+        </li>
+        
+        <li>
         <label>Avez-vous déjà été testé pour les infections sexuellement transmissibles (ist) ?</label>
         <input type="radio" id="oui" name="ist" value="Oui" required>
         <label for="oui">Oui</label>
         <input type="radio" id="non" name="ist" value="Non" required>
         <label for="non">Non</label><br><br> 
-            
+        </li>
+        
+        <li>
         <label>Le préservatif est-il efficace pour prévenir les grossesses et les ist ?</label>
         <input type="radio" id="oui" name="preservatif" value="Oui" required>
         <label for="oui">Oui</label><br>
         <input type="radio" id="non" name="preservatif" value="Non" required>
         <label for="non">Non</label><br><br>
-            
+        </li>
+        
+        <li>
         <label>Quelles sont les différentes maladies sexuellement transmissibles (mst) que vous connaissez ?</label>
             <input type="text" name="mst" required><br><br>
+        </li>
         
+        <li>
         <label>Avez-vous déjà consulter un professionnel de santé?</label>
            <input type="radio" id="oui" name="acces_sante" value="Oui" required>
             <label for="oui">Oui</label>
             <input type="radio" id="non" name="acces_sante" value="Non" required>
             <label for="non">Non</label><br><br>
+        </li>
         
+        <li>
         <label>Utilisez-vous toujours une protection lors de vos rapports sexuels ?</label>
             <input type="radio" id="oui" name="protection" value="Oui" required>
             <label for="oui">Oui</label>
             <input type="radio" id="non" name="protection" value="Non" required>
             <label for="non">Non</label><br><br>
+        </li>
         
+        <li>
         <label>Pensez-vous que l'éducation sexuelle est suffisante ?</label>
             <input type="radio" id="oui" name="education" value="Oui" required>
             <label for="oui">Oui</label>
             <input type="radio" id="non" name="education" value="Non" required>
             <label for="non">Non</label><br><br>
-
+        </li>
+        
+        
+        <li>
          <label>Comment amélioreriez-vous l'accès à l'éducation sexuelle et aux services de santé reproductive pour les jeunes ?</label>
         <input type="text" name="amelioration" required><br><br>
+        </li>
+        </ol>
             
         <input type="submit" value="Soumettre">         
         </form>
@@ -187,11 +323,5 @@ if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0',
                             'server.socket_port': int(os.environ.get('PORT', 8081))
                             })
-    conf = {
-        '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir':  'static'
-        }
-    }
     cherrypy.quickstart(MonSiteWeb(), config="tutoriel.conf")
     
