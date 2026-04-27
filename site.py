81d9a5fd99332e3fa1df6f09fbaebfd9d7bd30fd
Jiyce <joycetadaah26@gmail.com>
2026-04-27 07:22:58 +0000

    Add files via upload

--- site.py
+++ site.py
@@ -849,7 +849,8 @@ class MonSiteWeb(object):
             for sexe, reponse, nb in reponses:
                  if sexe not in data_par_sexe:
                     data_par_sexe[sexe] = []
-                    data_par_sexe[sexe].append((reponse, nb))
+                
+            data_par_sexe[sexe].append((reponse, nb))
             
             for sexe, liste in data_par_sexe.items():
                 html += f"<strong><h4>{sexe} :</h4></strong><ul>"
